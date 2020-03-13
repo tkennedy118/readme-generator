f89d3fd83dc32c52f83dfaf6a65c74b26abcfbb0
Tyler Kennedy <tkennedy118@gmail.com>
2020-03-12 20:56:44 -0500

    add prompt questions to an array

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
+const fs        = require("fs");
 const axios     = require("axios");
 const inquirer  = require("inquirer");
 
-const questions = [];
+const api       = require("./utils/api");
+const gm        = require("./utils/generateMarkdown");
+
+// questions about project
+const questions = [
+    {
+        type: "input",
+        name: "username",
+        message: "GitHub username:"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "Project title:",
+        default: "none"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Description of project:",
+        default: "none"
+    },
+    {
+        type: "choices",
+        name: "license",
+        message: ['ISC', 'MIT', 'Apache', 'GPL', 'Other'],
+        default: "ISC"
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "Command to install dependencies:",
+        default: "npm install"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Command to run tests:",
+        default: "npm test"
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "What should the user know about using this repository?",
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message: "What should the user know about contributing to the repositor?"
+    }
+];
 
 const writeToFile = (fileName, data) => {
 
